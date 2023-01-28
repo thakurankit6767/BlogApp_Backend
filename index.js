376a36dd5e5228a8357d706835e07c3cd7c3a6a7
thakurankit6767 <thakurankt6767@gmail.com>
2023-01-28 18:43:33 +0530

    done

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  ),
-    next();
-});
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", Router);

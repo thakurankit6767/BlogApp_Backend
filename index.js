04c7a8af88142aaf441dcb257747be74556ac478
thakurankit6767 <thakurankt6767@gmail.com>
2023-01-28 18:23:40 +0530

    done

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 //components
-import Connection from './database/db.js';
-import Router from './routes/route.js';
-
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  ),
+    next();
+});
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', Router);
-
+app.use("/", Router);
 
 const USERNAME = process.env.DB_CONNECT;
 
